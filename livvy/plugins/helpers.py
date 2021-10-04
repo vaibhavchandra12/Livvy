0567909cf81c8a0809db04621d916e2a087e5677
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-04 15:38:48 +0530

    Update helpers.py

--- livvy/plugins/helpers.py
+++ livvy/plugins/helpers.py
@@ -15,13 +15,13 @@ def is_admin(group_id: int, user_id: int):
     try:
         user_data = bot.get_chat_member(group_id, user_id)
         if user_data.status == 'administrator' or user_data.status == 'creator':
-            # print(f'is admin user_data : {user_data}')
+             print(f'is admin user_data : {user_data}')
             return True
         else:
-            # print('Not admin')
+             print('Not admin')
             return False
     except:
-        # print('Not admin')
+         print('Not admin')
         return False
 
        
@@ -31,10 +31,10 @@ def is_admin(group_id: int, user_id: int):
 @bot.on_callback_query(call_back_in_filter('help'))
 def callback_help(_,query):
 
-    # for x in help_message:
-    #         global helptext_
-    #         helptext_ = x['Help']
-    #         print(helptext_)
+     for x in help_message:
+             global helptext_
+             helptext_ = x['Help']
+             print(helptext_)
 
     
         
@@ -45,9 +45,9 @@ def callback_help(_,query):
             for x in help_message:
                 module = query.data.split(':')[1]
                 helptext_ = x[f'{module}_Help']
-                # helptext_ = x['Meme_Help']
+                 helptext_ = x['Meme_Help']
                 query.message.reply(helptext_)
-                # query.message.reply(x['Admin_Help'])
+                query.message.reply(x['Admin_Help'])
 
             msg = query.message
             callback_module_name = query.data.split(':')[1]
