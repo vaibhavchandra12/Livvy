04e2042e700e30d9cec4036f0ca14c5c85745c44
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-04 15:40:40 +0530

    Update helpers.py

--- livvy/plugins/helpers.py
+++ livvy/plugins/helpers.py
@@ -15,13 +15,13 @@ def is_admin(group_id: int, user_id: int):
     try:
         user_data = bot.get_chat_member(group_id, user_id)
         if user_data.status == 'administrator' or user_data.status == 'creator':
-             print(f'is admin user_data : {user_data}')
+            print(f'is admin user_data : {user_data}')
             return True
         else:
-             print('Not admin')
+            print('Not admin')
             return False
     except:
-         print('Not admin')
+        print('Not admin')
         return False
 
        
