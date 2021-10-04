ab01532165513c04b6b6e6e867c02a111394a2ad
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-04 18:03:44 +0530

    Update __init__.py

--- livvy/__init__.py
+++ livvy/__init__.py
@@ -9,5 +9,12 @@ livvycmd = Client(
     plugins=dict(root=f"livvy/plugins")
 )
 
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+
+from config import MONGO_URL
+
+mongo_client = MongoClient(MONGO_URL)
+db = mongo_client.livvy
+
 
 help_message = []
