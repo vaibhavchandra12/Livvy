02492d33e472c8d37beb1a52feab6e077832c7b5
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-04 14:47:42 +0530

    Update help.py

--- livvy/plugins/help.py
+++ livvy/plugins/help.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
-from pyrogram.types.bots_and_keyboards import callback_game
-from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
-from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup 
-from nksama import bot ,help_message
+from pyrogram import callback_game
+from pyrogram import InlineKeyboardButton
+from pyrogram import InlineKeyboardMarkup 
+from livvy import bot ,help_message
 
 @bot.on_message(filters.command('help'))
 def bothelp(_,message):
